0da3e5f3df8a1b248bac9a4c0b63179de390be45
Dianne Cook <visnut@gmail.com>
2014-01-09 11:20:00 -0600

    populating the web site

---
+++ stat585/code/1-read-data-extra.R
@@ -0,0 +1,92 @@
+library(reshape)
+library(plyr)
+library(ggplot2)
+library(maps)
+library(ggmap)
+
+temp.all<-read.table("../data/9641C_201112_raw.avg")
+dim(temp.all)
+colnames(temp.all)<-c("year", "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec", "ann")
+temp.all$stn<-temp.all$year%/%100000
+temp.all$year<-temp.all$year%%10000
+head(temp.all)
+
+stn.all<-read.table("http://cdiac.ornl.gov/ftp/ushcn_v2_monthly/ushcn-stations.txt", fill=T)
+stn.all<-stn.all[,c(1:3,5:6)]
+colnames(stn.all)<-c("stn", "lat", "lon", "state", "town")
+stn.all$name<-paste(stn.all$town, stn.all$state)
+head(stn.all)
+
+temp.all2<-merge(temp.all, stn.all, by="stn")
+dim(temp.all)
+dim(temp.all2)
+head(temp.all2)
+
+iowa <- subset(temp.all2, state=="IA")
+dim(iowa)
+head(iowa)
+
+iowa.melt <- melt(iowa[,-c(15, 19)], 
+    id=c("stn", "year", "lat", "lon", "name", "state"))
+
+colnames(iowa.melt)[7]<-"month"
+colnames(iowa.melt)[8]<-"temp"
+iowa.melt$temp <- as.numeric(as.character(iowa.melt$temp))
+iowa.melt$temp[iowa.melt$temp==-9999] <- NA
+iowa.melt$temp<-iowa.melt$temp/10
+
+iowa.melt$state <- factor(iowa.melt$state)
+iowa.melt$name <- factor(iowa.melt$name)
+iowa.melt$stn <- factor(iowa.melt$stn)
+
+summary(iowa.melt)
+summary(iowa.melt$month)
+
+temp.min <- min(iowa.melt$temp, na.rm=T)
+subset(iowa.melt, temp == temp.min)
+
+iowa.rngs <- ddply(iowa.melt, "stn", summarise, 
+                   mintemp = min(temp, na.rm=T), 
+                   maxtemp = max(temp, na.rm=T), 
+                   lon = unique(lon), 
+                   lat = unique(lat),
+                   name = unique(name))
+dim(iowa.rngs)
+head(iowa.rngs)
+qplot(lon, lat, data=iowa.rngs, colour=mintemp)
+qmap(location = 'iowa', zoom = 6, 
+     maptype = 'watercolor', source = 'stamen') +
+  geom_point(data=iowa.rngs, 
+             mapping=aes(x=lon, y=lat, colour=mintemp), size=5) 
+
+# Iowa prairies 
+newpbi <- read.csv("../data/newpbi.csv")
+dim(newpbi)
+newpbi[1,]
+newpbi.m <- melt(newpbi, id.var = c("Site", "Type"))
+head(newpbi.m)
+colnames(newpbi.m)[3] <- "Species"
+colnames(newpbi.m)[4] <- "Count"
+head(newpbi.m)
+qplot(Species, Count, data=newpbi.m) + coord_flip()
+
+# html
+src ="http://www.realclearpolitics.com/epolls/2012/president/us/republican_presidential_nomination-1452.html"
+doc <- htmlParse(src)
+str(doc)  # very terse
+print(doc)
+length(getNodeSet(doc, "//table"))
+tables <- getNodeSet(doc, "//table")
+table15 <- getNodeSet(tables[[15]], "tr")
+length(table15)
+
+table15[[1]]
+table15[[2]]
+table15[2:10]
+
+header <- sapply(getNodeSet(table15[[1]], "th"), xmlValue)
+rows <- t(sapply(table15[-1],function(x) xpathSApply(x,"td", xmlValue)))
+polls <- data.frame(rows)
+names(polls) <- header
+summary(polls) # still some work to do ...
+head(polls)
